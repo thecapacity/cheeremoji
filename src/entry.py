4cb66a306f5366bf89fc24999a671690d004e341
jay <thecapacity@users.noreply.github.com>
2025-01-04 19:41:09 -0500

    Update for CORS Preflight

--- src/entry.py
+++ src/entry.py
@@ -104,7 +104,7 @@ async def on_fetch(request, env):
         ("Content-Type", "application/json; charset=utf-8"),
         ("Access-Control-Allow-Origin", "*"),
         ("Access-Control-Allow-Methods", "GET, POST, OPTIONS"),
-        ("Access-Control-Allow-Headers", "Content-Type")
+        ("Access-Control-Allow-Headers", "Content-Type, Authorization")
     ]
 
     url = urlparse(request.url)
